afc171a2cf701aafdc325fc1c67e2147b8c8ad92
RaulS22 <rau1d3as@gmail.com>
2024-10-07 01:44:45 +0100

    add: mars training

--- Fourier_View/using_fourier.py
+++ Fourier_View/using_fourier.py
@@ -10,18 +10,29 @@ from fourier_and_heatmap import FourierAnalysis
 
 # Create an instance of the FourierAnalysis class
 
-filepath = 'data/lunar/training/data/S12_GradeA/'
+# Mars
+
+filepath = 'data/mars/training/data/'
 
 filenames = [file for file in os.listdir(filepath) if file.endswith('.mseed')]
-
-fourier_analysis = FourierAnalysis(filepath=filepath) 
-
+fourier_analysis = FourierAnalysis(filepath=filepath)
 fourier_analysis.load_data(filenames)
+fourier_analysis.plot_fourier()
+fourier_analysis.compute_spectrograms()
+fourier_analysis.plot_2d_heatmaps()
+fourier_analysis.plot_3d_heatmaps()
 
+
+# Moon
+
+filepath = 'data/lunar/training/data/S12_GradeA/'
+filenames = [file for file in os.listdir(filepath) if file.endswith('.mseed')]
+fourier_analysis = FourierAnalysis(filepath=filepath) 
+fourier_analysis.load_data(filenames)
 fourier_analysis.plot_fourier() #Plot the Fourier Transform of the seismic data
-
 fourier_analysis.compute_spectrograms() #Compute spectrograms for the loaded data
-
 fourier_analysis.plot_2d_heatmaps() #Plot 2D heatmaps for the spectrograms
-
 fourier_analysis.plot_3d_heatmaps() #Plot 3D heatmaps for the spectrograms
+
+
+
